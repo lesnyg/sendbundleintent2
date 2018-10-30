60e73d11ba92e7a859d1765d0703212a33d35fbb
이선영 <lsy79@hanmail.net>
2018-10-30 11:46:58 +0900

    second commit

--- app/src/main/java/com/lesnyg/edu/sendbundleintent2/MainActivity.java
+++ app/src/main/java/com/lesnyg/edu/sendbundleintent2/MainActivity.java
@@ -1,13 +1,31 @@
 package com.lesnyg.edu.sendbundleintent2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String text = ((Button)v).getText().toString();
+        Bundle bundle = new Bundle();
+        bundle.putString("singleName",text);
+        Intent intent = new Intent(this,secondActivity.class);
+        intent.putExtras(bundle);
+        startActivity(intent);
+
     }
 }
--- app/src/main/java/com/lesnyg/edu/sendbundleintent2/secondActivity.java
+++ app/src/main/java/com/lesnyg/edu/sendbundleintent2/secondActivity.java
@@ -2,6 +2,7 @@ package com.lesnyg.edu.sendbundleintent2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class secondActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class secondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Bundle bundle = getIntent().getExtras();
+        TextView textView = (TextView)findViewById(R.id.singleName);
+        textView.setText(bundle.getString("singleName","No Data"));
     }
 }
